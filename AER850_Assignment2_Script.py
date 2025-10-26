ac2b5ffe56aa917bf4aeb738211866abc2a5f7f9
deepinder-saron <deepinder.saron@torontomu.ca>
2025-10-26 12:50:54 -0400

    Model 1 Stuff Done
    Following done for Model 1:
    - Neural Network Architecture Design
    - Hyperparameter Analysis
    - Model Evaluation

--- AER850_Assignment2_Script.py
+++ AER850_Assignment2_Script.py
@@ -10,9 +10,9 @@ import pickle
 
 '''2.1: Data Processing'''
 
-# We need to define the image size which is suppose to be 500 x 500
-img_height = 500
-img_width = 500
+# We need to define the image size which is suppose to be 500 x 500, but we have to compress it so when training and testing occurs, it doesn't run for lik 12 hours
+img_height = 96
+img_width = 96
 batch_size = 32 # determines how many images are processed in one batch when training
 
 # Setting the directory paths for train and validation folder
@@ -86,3 +86,138 @@ test_generator = test_datagen.flow_from_directory(
 # Checking if the labels to see if it split correctly
 
 print("Class indices: ", train_generator.class_indices)
+
+'''2.2a: Neural Network Architecture Design For FIRST MODEL'''
+
+# Creating a sequential model where you can add each layer individually 
+
+model1 = Sequential()
+
+# adding layers to the model
+
+# The convolution layer is responsible for extracting features from the input data 
+# by applying various operations. This layer helps the model identify different features
+# such as simple shapes within the image. Each of the filters (or kernels) detects a specific
+# type of feature like an edge or a horizontal line within the input data. For this project,
+# the convolution layers will use the ReLU activation function.
+
+# The max pooling layer reduces the spatial dimensions of the input feature maps while
+# retaining the important parts. Basically just makes the model more computationally efficient 
+# and robust to small translations or distortions
+
+# Batch Normalization allows the model to stabilize and also speeds up he training
+
+model1.add(Conv2D(32, (3, 3), activation = 'relu', input_shape = (96, 96, 1), kernel_regularizer = l2(0.001)))
+model1.add(BatchNormalization())
+model1.add(MaxPooling2D(pool_size = (2, 2)))
+
+# increase in filters in order to capture and learn more complex features as we go
+model1.add(Conv2D(64, (3, 3), activation = 'relu', kernel_regularizer = l2(0.001)))
+model1.add(BatchNormalization())
+model1.add(MaxPooling2D(pool_size = (2, 2)))
+
+model1.add(Conv2D(128, (3, 3), activation = 'relu', kernel_regularizer = l2(0.001)))
+model1.add(BatchNormalization())
+model1.add(MaxPooling2D(pool_size = (2, 2)))
+
+# flatten layer converts the 2D feature maps into a 1D vector to be passed into
+# the fully connected layers
+model1.add(BatchNormalization())
+model1.add(Flatten())
+
+# dense layer adds a fully connected layer with 128 neurons, this where it will
+# begin to connect every input from previous neurons to the ones in this layer
+# and begin to learn patterns and relationships
+model1.add(Dense(128, activation = 'relu'))
+
+# dropout layer will randomly set a certain % of input units to 0 during training
+# in order to prevent overfitting 
+model1.add(Dropout(0.3))
+
+# final output layer that has 3 neurons (one for each class)
+# softmax activation function used to convert output into a probability distribution
+# across the classes
+model1.add(Dense(3, activation = 'softmax'))
+
+# compiling the model
+model1.compile(optimizer = Adam(learning_rate = 0.0005), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# summary of the model which shows the number of layers, output shape of each layer, 
+# and total parameters in the model
+model1.summary()
+
+early_stopping = EarlyStopping(
+    monitor = "val_accuracy",  # Metric to monitor
+    patience = 20,  # Number of epochs to wait for improvement
+    restore_best_weights = True,  # Restore the weights of the best model
+)
+
+reduce_lr = ReduceLROnPlateau(
+    monitor = "val_loss",
+    factor = 0.5,
+    patience = 5,
+    min_lr = 1e-6
+)
+
+'''2.3a: Model Training For FIRST MODEL'''
+
+# training the first model
+
+# steps_per_epoch defines how many batchs to process before considering an epoch to be complete
+
+# epochs is the number of times the model goes through the entire training set
+
+# validation_data uses the validation generator to evaluate the model after each epoch
+
+# validation_steps defines the number of batches to process from the validation
+# generator at each epoch
+
+trained1 = model1.fit(
+    train_generator,
+    epochs = 100,
+    validation_data = valid_generator,
+    callbacks = [early_stopping, reduce_lr]
+    )
+
+# Saving Model 1
+model1.save('AER850_Project2/Model1/trained_model1_{0}.h5')
+
+with open('AER850_Project2/Model1_history.pkl', 'wb') as file:
+    pickle.dump(trained1.history, file)
+    
+# Opening Model 1 (if needed)
+with open('AER850_Project2/Model1_history.pkl', 'rb') as f:
+    history = pickle.load(f)
+
+
+# plotting model 1
+plt.figure(figsize = (12 ,5))
+
+# accuracy plot
+plt.subplot(1, 2, 1)
+plt.plot(history['accuracy'], label = 'Training Accuracy')
+plt.plot(history['val_accuracy'], label = 'Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+# loss plot
+plt.subplot(1, 2, 2)
+plt.plot(history['loss'], label = 'Training Loss')
+plt.plot(history['val_loss'], label = 'Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+'''2.4a: Model Evaluation For FIRST MODEL'''
+
+# model 1
+test_loss, test_accuracy = model1.evaluate(test_generator, verbose = 2)
+
+# Print the results
+print("Test Loss 1:", test_loss)
+print("Test Accuracy 1:", test_accuracy)
